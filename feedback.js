f62fe6ae51e6be9fa154e43bdcb1b458527d47df
alex-ly <alex.ly@uoit.net>
2018-03-10 16:22:21 -0500

    Delete feedback.js

--- feedback.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-  console.log("Feedback given");
-  var editor = ace.edit("editor");
-  editor.setTheme("ace-master/lib/ace/theme/monokai");
-  editor.session.setMode("ace-master/lib/ace/mode/javascript");
-  console.log('file');
-}
